5f82b07146d67d00175a34e581b2a1249bc1e0cf
Josh Woodcock <josh@j0w0.com>
2020-09-18 03:57:04 -0500

    Implement CRUD functionality using Class-Based Views

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('playlists/', views.playlists_index, name='index'),
-    path('playlists/<int:playlist_id>/', views.playlists_detail, name='detail')
+    path('playlists/<int:playlist_id>/', views.playlists_detail, name='detail'),
+    path('playlists/create/', views.PlaylistCreate.as_view(),
+         name='playlists_create'),
+    path('playlists/<int:pk>/update',
+         views.PlaylistUpdate.as_view(), name='playlists_update'),
+    path('playlists/<int:pk>/delete',
+         views.PlaylistDelete.as_view(), name='playlists_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Playlist
 
 # Create your views here.
@@ -20,3 +21,20 @@ def playlists_index(request):
 def playlists_detail(request, playlist_id):
     playlist = Playlist.objects.get(id=playlist_id)
     return render(request, 'playlists/detail.html', {'playlist': playlist})
+
+
+class PlaylistCreate(CreateView):
+    model = Playlist
+    fields = '__all__'
+    success_url = '/playlists/'
+
+
+class PlaylistUpdate(UpdateView):
+    model = Playlist
+    fields = '__all__'
+
+
+class PlaylistDelete(DeleteView):
+    model = Playlist
+    fields = '__all__'
+    success_url = '/playlists/'
